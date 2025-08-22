363fce3d4bbcb1765c09cc621ad2979e55df2159
riveros <julianrivery12@gmail.com>
2025-08-22 14:07:45 -0500

    v0.7 vistaShowCompletaUpdateTemplateAdminUpdateClaseProductoController_julianrivers_22082025

--- src/main/java/com/sena/ecommerce/controller/ProductoController.java
+++ src/main/java/com/sena/ecommerce/controller/ProductoController.java
@@ -1,11 +1,14 @@
 package com.sena.ecommerce.controller;
 
+import java.util.Optional;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -29,7 +32,7 @@ public class ProductoController {
 		return "productos/show";
 	}
 
-	@GetMapping("/save")
+	@GetMapping("/vreate")
 	public String create() {
 		return "productos/create";
 	}
@@ -41,6 +44,35 @@ public class ProductoController {
 		producto.setUsuario(u);
 		productoservice.save(producto);
 		return "redirect:/productos";
+	}
+
+	// metodo para el formulario de edicion de productos
+	@GetMapping("/edit/{id}")
+	public String edit(@PathVariable Integer id, Model model) {
+		Producto p = new Producto();
+		// nos retorna la busqueda de un objeto de tipo producto con el id
+		Optional<Producto> op = productoservice.get(id);
+		p = op.get();
+		LOGGER.warn("Busqueda de producto por id{}", p);
+		model.addAttribute("productos", p);
+		return "productos/edit";
+	}
+
+	// metodo de actualizacion de datos
+	@PostMapping("/update")
+	public String update(Producto producto) {
+		LOGGER.info("Este es el objeto del producto a actualizar en la DB{}", producto);
+		Usuario u = new Usuario(1, "", "", "", "", "", "", "", "");
+		producto.setUsuario(u);
+		productoservice.update(producto);
+		return "redirect:/productos";
+	}
+
+	// metodo para eliminar con id un producto
+	@GetMapping("/delete/{id}")
+	public String delete(@PathVariable  Integer id) {
+		productoservice.delete(id);
+		return "";
 	}
 
 }
